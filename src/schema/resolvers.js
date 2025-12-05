98b88a4bad671f1daa9feccada16789eb57b84a1
Ganu87 <shelke.ganesh87@gmail.com>
2025-12-05 12:09:53 +0530

    changes sceham name and add script

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,60 +1,60 @@
-import Student from "../models/Student.js";
-import Course from "../models/Course.js";
-import Enrollment from "../models/Enrollment.js";
+import StudentSchema from "../models/Student.js";
+import CourseSchema from "../models/Course.js";
+import EnrollmentSchema from "../models/Enrollment.js";
 
 
 export const resolvers = {
   Student: {
     courses: async (parent) => {
-      const enrollments = await Enrollment.find({ studentId: parent.id });
-      return await Course.find({ _id: { $in: enrollments.map(e => e.courseId) } });
+      const enrollments = await EnrollmentSchema.find({ studentId: parent.id });
+      return await CourseSchema.find({ _id: { $in: enrollments.map(e => e.courseId) } });
     }
   },
 
   Course: {
     students: async (parent) => {
-      const enrollments = await Enrollment.find({ courseId: parent.id });
-      return await Student.find({ _id: { $in: enrollments.map(e => e.studentId) } });
+      const enrollments = await EnrollmentSchema.find({ courseId: parent.id });
+      return await StudentSchema.find({ _id: { $in: enrollments.map(e => e.studentId) } });
     }
   },
 
   Enrollment: {
-    student: async (parent) => await Student.findById(parent.studentId),
-    course: async (parent) => await Course.findById(parent.courseId)
+    student: async (parent) => await StudentSchema.findById(parent.studentId),
+    course: async (parent) => await CourseSchema.findById(parent.courseId)
   },
 
   Query: {
-    getStudents: async () => await Student.find({}),
-    getCourses: async () => await Course.find({}),
-    getEnrollments: async () => await Enrollment.find({}),
+    getStudents: async () => await StudentSchema.find({}),
+    getCourses: async () => await CourseSchema.find({}),
+    getEnrollments: async () => await EnrollmentSchema.find({}),
 
-    getStudent: async (_, { id }) => await Student.findById(id),
-    getCourse: async (_, { id }) => await Course.findById(id),
+    getStudent: async (_, { id }) => await StudentSchema.findById(id),
+    getCourse: async (_, { id }) => await CourseSchema.findById(id),
 
     getStudentCourses: async (_, { id }) => {
-      const enrollments = await Enrollment.find({ studentId: id });
-      return await Course.find({ _id: { $in: enrollments.map(e => e.courseId) } });
+      const enrollments = await EnrollmentSchema.find({ studentId: id });
+      return await CourseSchema.find({ _id: { $in: enrollments.map(e => e.courseId) } });
     },
 
     getCourseStudents: async (_, { id }) => {
-      const enrollments = await Enrollment.find({ courseId: id });
-      return await Student.find({ _id: { $in: enrollments.map(e => e.studentId) } });
+      const enrollments = await EnrollmentSchema.find({ courseId: id });
+      return await StudentSchema.find({ _id: { $in: enrollments.map(e => e.studentId) } });
     }
   },
 
   Mutation: {
     createStudent: async (_, { name, age }) => {
-      const student = new Student({ name, age });
+      const student = new StudentSchema({ name, age });
       return await student.save();
     },
 
     createCourse: async (_, { title, description }) => {
-      const course = new Course({ title, description });
+      const course = new CourseSchema({ title, description });
       return await course.save();
     },
 
     enrollStudent: async (_, { studentId, courseId, grade }) => {
-      const enrollment = new Enrollment({ studentId, courseId, grade });
+      const enrollment = new EnrollmentSchema({ studentId, courseId, grade });
       return await enrollment.save();
     }
   }
